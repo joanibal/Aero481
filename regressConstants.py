6ca0c29eb6745132ae4de0bda6728209086908b8
hillrebe <31936951+hillrebe@users.noreply.github.com>
2017-09-13 16:45:51 -0400

    Calculate A and B

---
+++ regressConstants.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+WTO = np.array([92500,  73000, 174200, 99600])
+WE = np.array([50861,  36100, 102100, 54000])
+
+A = np.vstack([np.log10(WE), np.ones(len(WE))]).T
+
+B, A = np.linalg.lstsq(A, np.log10(WTO))[0]
+
+print(A, B)
+
+
+plt.plot(np.log10(WE), np.log10(WTO), 'o', label='Original data', markersize=10)
+plt.plot(np.log10(WE), B*np.log10(WE) + A, 'r', label='Fitted line')
+plt.legend()
+plt.show()
