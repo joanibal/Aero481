30030ece839a0254c8f7d0adfa77d327d48ec8e3
Josh Anibal <joanibal@umich.edu>
2017-09-13 17:14:54 -0400

    w_o fixed

--- weight_estimation.py
+++ weight_estimation.py
@@ -1,15 +1,15 @@
 import numpy as np
-import math
+# import math
 
 #human weights (project specs)
-w_crew = 3*(180+60)		#lbs (crew weight + luggage)
-w_payload = 8*(180+60)	#lbs (passenger weight + luggage)
+w_crew = 3.0*(180+60)		#lbs (crew weight + luggage)
+w_payload = 8.0*(180+60)	#lbs (passenger weight + luggage)
 
 #flight conditions (project specs)
 M = 0.85 			#cruise mach
 R = 5000 + 200		#nautical miles (range + divert extra)
-LD = 16				#based on Roskam (L/D)
-c = 0.5				#1/hrs, based on Roskam (TSFC)
+LD = 11				#based on Roskam (L/D)
+c = 0.75				#1/hrs, based on Roskam (TSFC)
 
 #fuel fractions (ROSKAM)
 ff1 = 0.99		#warmup
@@ -23,34 +23,49 @@ ff7 = 0.992		#landing
 a_sound = 574		#knots (speed of sound @40 K-ft)
 V_kts = M*a_sound	#knots = nm/hr (flight speed)
 
-print(V_kts)
+# print(V_kts)
 
 #calculating cruise fuel fraction
 ff5 = np.exp(-R/LD*c/V_kts)	#solving breguet equation
 
-print(ff5)
+# print(ff5)
 
 #total fuel fraction
 ff = ff1*ff2*ff3*ff4*ff5*ff6*ff7
-print(ff)
+# print(ff)
 
-#Roskam Equation constants
+#Raymer Equation constants
 A = 1.02
 C = -0.06
 
 #solving for MTOW
-w_0 = 80000		#lbs (weight guess)
+w_0 = 80000.0		#lbs (weight guess)
 
 tolerance = 0.1				#Adjust for acccuracy
 converged = 0				#False
 
+# print(C)
+# print(A*w_0)
+# print(w_0**C)
+# print(A*w_0**C)
+
 while (converged == 0):
+	# print(w_0)
 	emptyWeightFraction = A*w_0**C
 	fuelFraction = ff
 	w_0new = (w_crew+w_payload)/(1-fuelFraction-emptyWeightFraction)
    
+	# print((w_crew+w_payload))
+	# print(fuelFraction,emptyWeightFraction)
+	# print((1-fuelFraction-emptyWeightFraction))
+
+
 	if (abs(w_0new - w_0) <= tolerance):
    		converged = 1
-	w_0 = w_0new
+	w_0 += 0.1*(w_0new - w_0)
+	# w_0 = w_0new
+
+	# converged = 1
+
 	
 print(w_0)
