29cd4d82a434371dca2aef6c2c760b535602dbdd
aalu1418 <31937420+aalu1418@users.noreply.github.com>
2017-09-14 13:52:27 -0400

    regression update

--- regression.py
+++ regression.py
@@ -1,11 +1,12 @@
-import numpy as np
-import scipy
+def regression():
+	import numpy as np
+	import scipy
 
-WTO = np.array([92500, 69600, 73000, 99600])
-WE = np.array([50861, 43500, 36100, 54000])
+	WTO = np.array([92500, 69600, 73000, 99600])
+	WE = np.array([50861, 43500, 36100, 54000])
 
-(a_lin, b_lin)= np.polyfit(np.log10(WTO),np.log10(WE),1)
+	(a_lin, b_lin)= np.polyfit(np.log10(WTO),np.log10(WE),1)
 
-c = a_lin-1
-a = 10**b_lin
-print(a, c)
+	c = a_lin-1
+	a = 10**b_lin
+	return a, c
