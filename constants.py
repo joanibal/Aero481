8a4673cbc0fcf856c009bca97a8ae1fdf20f719c
Josh Anibal <joanibal@umich.edu>
2017-09-20 16:27:07 -0400

    joanibal

---
+++ constants.py
@@ -0,0 +1,24 @@
+runLength = 4948   # ft 
+alt = 400000       # ft 
+R = 5200           # nMi
+machCruise = 0.85  
+SFC = 0.657        # 1/hr
+
+numEngines = 2     
+
+runLength_Denver = 
+density_denver = 
+density_ceiling = 
+
+ceiling = 51000     # ft
+T_Ceiling = 216.650  # K
+T_SL
+P_Ceiling = 11053.0
+P_SL
+
+Density_Ceilng = 0.178     # kg/M^3
+Density_SL  = 1.225       # kg/M^3 
+a_Ceiling = 573.57          # knots 
+
+
+
---
+++ sizing.py
@@ -0,0 +1,11 @@
+from constants import constants
+import numpy as np
+
+
+
+np.linspace(0 350)
+
+
+# crusie 
+T_W_cruise = 1.0/(0.2826**0.6)*(228.8*0.01597)/W_S + (W_S)*1/(228.8*np.PI)
+
