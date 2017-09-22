3be94ff5bf8d18e57f6b08f0975630b9b352fc29
Josh Anibal <joanibal@umich.edu>
2017-09-22 14:07:20 -0400

    fix part 2

--- constants.py
+++ constants.py
@@ -13,15 +13,9 @@ numEngines = 2
 
 ceiling = 51000     # ft
 T_Ceiling = 216.650  # K
-<<<<<<< HEAD
 # T_SL
 P_Ceiling = 11053.0  # Pa
 # P_SL
-=======
-#T_SL
-P_Ceiling = 11053.0
-#P_SL
->>>>>>> 0c2db575e79e6802058ef889290bba455b760164
 
 Density_Ceilng = 0.178     # kg/M^3
 Density_SL  = 1.225       # kg/M^3 
