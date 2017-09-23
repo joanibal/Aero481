521d870d3203c5c5e7250cf385ba8b41b84b011c
Josh Anibal <joanibal@umich.edu>
2017-09-23 16:22:02 -0400

    added labels

--- sizing.py
+++ sizing.py
@@ -41,7 +41,7 @@ for i in range(0,6):
 
 # Cd_0 = 0.01597
 # Cd_0_climb = 0.01597
-N = 100
+N = 10000
 W_S = np.linspace(0, 350, N)
 # CL_max = 2.5
 
@@ -69,12 +69,26 @@ print W_S_landing
 
 
 
-plt.plot(W_S, np.ones(N)*T_W_ceiling, '--')
-plt.plot(W_S, T_W_cruise, '--')
-plt.plot(W_S, T_W_Takeoff, '--')
-for i in range(0,6):
-	plt.plot(W_S, np.ones(np.shape(W_S))*TW_corrected_array[i],'--')
+ceiling, = plt.plot(W_S, np.ones(N)*T_W_ceiling, label='Ceiling')
+cruise, = plt.plot(W_S, T_W_cruise, label='Cruise')
+takeoff, = plt.plot(W_S, T_W_Takeoff, label='Takeoff')
+A = []
 
-plt.plot([W_S_landing, W_S_landing], [ 0, 1], '--')
+for i in range(0,6):
+	A.append(plt.plot(W_S, np.ones(np.shape(W_S))*TW_corrected_array[i],'--', label=('Climb '+str(i))))
+
+landing, = plt.plot([W_S_landing, W_S_landing], [ 0, 1])
+
+a = np.logical_and(T_W_cruise>=TW_corrected_array[5], T_W_Takeoff<=TW_corrected_array[5])
+b = np.logical_and(np.logical_not(a), T_W_Takeoff<=TW_corrected_array[5])
+c = np.logical_and(T_W_Takeoff>=TW_corrected_array[5], W_S<=W_S_landing)
+
+plt.fill_between(W_S,T_W_cruise,1,where=a     ,interpolate=True, color='b')
+plt.fill_between(W_S,np.ones(np.shape(W_S))*TW_corrected_array[5],1,where=b,interpolate=True, color='b')
+plt.fill_between(W_S,T_W_Takeoff,1,where=c,interpolate=True, color='b')
+
 plt.axis((W_S[0], W_S[-1], 0, 1))	
+
+plt.legend(handles=[ceiling, cruise, takeoff, landing, A[0][0], A[1][0], A[2][0], A[3][0], A[4][0], A[5][0]])
+plt.legend(loc = 'upper right')
 plt.show()
