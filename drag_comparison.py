729968307e7799607e6b50d176e30106ac07d3fd
hillrebe <31936951+hillrebe@users.noreply.github.com>
2017-09-22 22:01:16 -0400

    edited to generalize

--- drag_comparison.py
+++ drag_comparison.py
@@ -5,12 +5,10 @@ ARfact = 1.05 #aspect ratio factor
 reffact = 1.05 #reference area factor
 
 # constants given initial geometry
-CD0 = 0.0250260642922
-CL = 0.4527
 AR = 9
 
-CDi_init = ((CL**2)*(1.05+0.007*np.pi*AR))/(np.pi*AR) # initial C_D induced
-CDi_new = ((CL**2)*(1.05+0.007*np.pi*AR*ARfact))/(np.pi*AR*ARfact) #C_D induced with the increased aspect ratio
+CDi_init = (1.05+0.007*np.pi*AR)/(np.pi*AR) # initial C_D induced, simplified to exclude values that would cancel out with new drag
+CDi_new = (1.05+0.007*np.pi*AR*ARfact)/(np.pi*AR*ARfact) #C_D induced with the increased aspect ratio, simplified to exclude values that would cancel out with initial drag
 
 Dratio = (CDi_new*reffact)/CDi_init #new drag/initial drag after simplification
 Dpercent = (Dratio-1)*100 # %change in drag
