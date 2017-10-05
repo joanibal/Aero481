7d2cad07607f9dc0d11bd9a2abf818aa18eec46b
hillrebe <31936951+hillrebe@users.noreply.github.com>
2017-10-05 15:05:10 -0400

    made general

--- Svt_calc.py
+++ Svt_calc.py
@@ -1,23 +1,23 @@
 from constants import *
 import numpy as np
 
-root = 14
+root = 14 #ft
 ratio = 0.7
-sweep = 0.628319 #radians
+sweep = 0.628319 #radians (36 degrees)
 ratio2 = 0.66666666666
 MAC1 = (root + ratio*root - (root*root*ratio)/(root+root*ratio))
 MAC = MAC1*ratio2
 x = MAC*np.tan(sweep)
-Lvt = 40.48-root+x
-print(Lvt)
+Lvt = (fuse_length-CG)-root+x
+print(Lvt) #in ft
 
 Lvt = Lvt*0.3048 #moment arm in m
 Svt = c_VT*b*Sref/Lvt
 print(Svt)
 
-base = root*0.3048
-tip = base*ratio
-print base, tip
+base = root*0.3048 #convert to m
+tip = base*ratio #m
+print base
 span = 2*Svt/(base+tip)
 print span
 
