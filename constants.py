78588d30667c4de5d48d002f3c7654284c6ab7d0
saifjam <saifj@umich.edu>
2017-10-14 17:53:19 -0400

    dynamic pressure update

--- constants.py
+++ constants.py
@@ -9,6 +9,17 @@ SFC = 0.657        							# 1/hr
 L_D = 15 									#used in weight_estimation
 speed_kts = 566.7279
 
+W = MTOW*9.81                               # Lift equals weight
+M = 0.85                                    # Cruise Mach numnber
+T_C = -56.5                                 # Temperature (Celcius)
+T = T_C + 273.15                            # Temperature (Kelvin)
+p = 18822.69                                # Absolute Pressure (Pa)
+R = 287                                     # Gas Constant (J/kgK)
+rho = p/(R*T)                               # Air Density (kg/m^3)
+a = math.sqrt(1.4*R*T)                      # Speed of Sound (m/s)
+u = M*a                                     # Airspeed (m/s)
+q = 0.5*rho*u**2                            # Dynamic Pressure
+
 # G550 specs
 # machCruise = 0.8
 # SFC = 0.65 # 1/hr
