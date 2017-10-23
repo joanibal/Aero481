8d7c660264e23dda42a14f1df26afe492e2f8dbc
saifjam <saifj@umich.edu>
2017-10-22 20:51:59 -0400

    Calc M Crit

--- Aerodynamics/calcMCrit.py
+++ Aerodynamics/calcMCrit.py
@@ -5,7 +5,8 @@ import os,sys,inspect
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from sympy.solvers import solve
 from sympy import Symbol
-
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 import math
 import numpy as np
 import constants
@@ -20,12 +21,14 @@ D = constants.c_root
 mu = 2.995e-7*47.88026
 U = constants.u
 Re = rho*U*D/mu
-#print("Re (Root Chord):" + str(Re))
+print("Re (Root Chord):" + str(Re))
 
 #XFOIL minimum Cp at Alpha = 0
-cp_0 = 0.9965
+cp_0 = -0.9965
 cpmin = cp_0/math.sqrt(1-M_cruise**2)
 cpcrit = cpmin
+func = lambda M : 2/(gamma*M**2)*(((1+((gamma-1)/2)*M**2)/(1+(gamma-1)/2))**(gamma/(gamma-1))-1)-cpcrit
+M_guess = 1
+M_solution = fsolve(func, M_guess)
 
-M= Symbol('M')
-print(solve(2/(gamma*M**2)*(((1+((gamma-1)/2)*M**2)/(1+(gamma-1)/2))**(gamma/(gamma-1))-1)-cpcrit, M))
+print("Critical Mach Number: " + str(M_solution))
