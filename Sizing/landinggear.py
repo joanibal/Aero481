0c00dfa3fd43d8de51d55414d8b962079e02ae17
hillrebe <31936951+hillrebe@users.noreply.github.com>
2017-10-23 16:32:10 -0400

    added braking energy

--- Sizing/landinggear.py
+++ Sizing/landinggear.py
@@ -5,8 +5,10 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import constants as consts
 import numpy as np
 
-def landinggear(nose, main, fwdCG, aftCG, H, W):
-        #inputs: nose gear position, main gear position, forward CG position, aft CG position, height of CG, MTOW
+def landinggear(nose, main, fwdCG, aftCG, H, W, Wlanding, Vstall):
+        #inputs: nose gear position, main gear position, forward CG position, aft CG position, height of CG, MTOW, landing weight, stall speed
+        #units: ft, lbs, ft/s
+
 
     g = 32.2
 
@@ -23,5 +25,6 @@ def landinggear(nose, main, fwdCG, aftCG, H, W):
 
     wheel_load = 1.07*1.25*maxSL/wheels_main
     wheel_load_nose = 1.07*1.25*DBL_nose/wheels_nose
+    KE_braking = 0.5*Wlanding*Vstall**2/(g*wheels_main)
 
-    return (Ma/B), (Mf/B), wheel_load, wheel_load_nose
+    return (Ma/B), (Mf/B), wheel_load, wheel_load_nose, KE_braking
