8834571d4543fca42f1192e7d415dde49ef0ab79
saifjam <saifj@umich.edu>
2017-10-22 20:20:05 -0400

    Critical Mach Num

---
+++ Aerodynamics/calcMCrit.py
@@ -0,0 +1,31 @@
+#Calculate the Critical Mach Number for Airfoil
+
+import os,sys,inspect
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from sympy.solvers import solve
+from sympy import Symbol
+
+import math
+import numpy as np
+import constants
+
+a = constants.a
+gamma = 1.4 			#Cp/Cv (ideal)
+M_cruise = constants.M
+
+#Calculate Reynolds Number at Cruise
+rho = constants.rho
+D = constants.c_root
+mu = 2.995e-7*47.88026
+U = constants.u
+Re = rho*U*D/mu
+#print("Re (Root Chord):" + str(Re))
+
+#XFOIL minimum Cp at Alpha = 0
+cp_0 = 0.9965
+cpmin = cp_0/math.sqrt(1-M_cruise**2)
+cpcrit = cpmin
+
+M= Symbol('M')
+print(solve(2/(gamma*M**2)*(((1+((gamma-1)/2)*M**2)/(1+(gamma-1)/2))**(gamma/(gamma-1))-1)-cpcrit, M))
