d0df582abdd87d03dbedcff9443638d504e17923
saifjam <saifj@umich.edu>
2017-11-17 21:36:45 -0500

    Updated graphs

--- Aerodynamics/plotAeroTrades.py
+++ Aerodynamics/plotAeroTrades.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import string
 
 filenames = ["results_sweep.txt", "results_taper.txt", "results_ar.txt", "results_sref.txt"]
-xlabels = ["Sweep", "Taper Ratio", "Aspect Ratio", "Sref"]
+xlabels = ['Sweep, ' + '$\Lambda$' + ' (deg)', 'Taper, ' + '$\lambda$', 'Aspect Ratio, AR', '$S_{ref}$']
 ylabels = ["CL/CD", "CL/CD", "CL/CD", "CL/CD"]
-x_min = [0, 0, 1, 900]
-x_max = [40, 1, 16, 2500]
+titles = ["Sweep", "Taper Ratio", "Aspect Ratio", "Reference Area"]
+#x_min = [0, 0, 1, 900]
+#x_max = [40, 1, 16, 2500]
 
 for j in range(len(filenames)):
 	file = open(filenames[j], "r")
@@ -15,16 +16,20 @@ for j in range(len(filenames)):
 	x = []
 	y = []
 
-	for n in range(1, len(flines)):
+	for n in range(0, len(flines)):
 		txt = string.split(flines[n]) 
 		x.append(float(txt[ 0]))
 		y.append(float(txt[ 1]))
 
-	plt.axis('equal')
+	plt.axis('auto')
 	plt.plot(x,y, linewidth=2)
-	#plt.xlim(x_min[j], x_max[j])
-	axes = plt.gca()
-	axes.set_xlim([x_min[j],x_max[j]])
+	plt.title(titles[j])
+	plt.ylabel('${C_L}/{C_D}$')
 	plt.xlabel(xlabels[j])
-	plt.ylabel(ylabels[j])
+	#plt.xlim(x_min[j], x_max[j])
+	#axes = plt.gca()
+	#axes.set_xlim([x_min[j],x_max[j]])
+	#plt.xlabel(xlabels[j])
+	#plt.ylabel(ylabels[j])
 	plt.show()
+
