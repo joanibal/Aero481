e0cf2864053940496cc9511d7ef59b431aae9c33
hillrebe <31936951+hillrebe@users.noreply.github.com>
2017-12-08 18:26:37 -0500

    runway length

--- Aerodynamics/takeoff_landing_length.py
+++ Aerodynamics/takeoff_landing_length.py
@@ -2,17 +2,17 @@ import os,sys,inspect
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-import constants as consts
-from Weight.weight_buildup import prelim_weight
+import j481 as consts
+from Weight.weight_refined import prelim_weight
 
-w_0, w_f = prelim_weight(consts.Sref*10.7639, consts.thrust_req, consts)
+w_0, w_f, _ = prelim_weight(consts.Sref, consts.thrust_req, consts)
 Clmax_t = consts.CL['max']['takeoff']
 Clmax_l = consts.CL['max']['landing']
 
-TOP = (w_0/(consts.Sref*10.7639))/(1*Clmax_t*(consts.thrust_req/w_0))
+TOP = (w_0/(consts.Sref))/(1*Clmax_t*(consts.thrust_req/w_0))
 BFL = 37.5*TOP
 
-ground_roll = 80*(w_0/(consts.Sref*10.7639))*(1/(1*Clmax_l))
+ground_roll = 80*(w_0/(consts.Sref))*(1/(1*Clmax_l))
 landing_field = 1.67*(ground_roll+1000)
 
 print BFL, ground_roll, landing_field
